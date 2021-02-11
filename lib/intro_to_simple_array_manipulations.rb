f7c6454546b5badd51708eda699968829d27a213
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-11 00:09:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,24 @@
 def using_push(array, string)
-  array.push(string)
+  return array.push(string)
 end
 puts using_push(['hi', 'there', 'sir'], '!')
 
 def using_unshift(array, string)
-  array.unshift(string)
+  return array.unshift(string)
 end
 puts using_push(['hi', 'there', 'sir'], '!')
+
+def using_pop(array)
+ return array.pop
+end
+puts using_push(['hi', 'there', 'sir'])
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+puts using_push(['hi', 'there', 'sir'])
+
+def using_shift(array)
+  return array.shift
+end
+puts using_push(['hi', 'there', 'sir'])
